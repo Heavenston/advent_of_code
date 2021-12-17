087d0b0a0e0f5bc25d8c573ee4154e0345feb2a2
Heavenston <malolegendrelemaire@@gmail.com>
2021-12-17 05:33:20 +0100

    Updated library

--- 2021/ADayX/Python/part1.py
+++ 2021/ADayX/Python/part1.py
@@ -1,7 +1,9 @@
-from collections.abc import Callable
+from collections.abc import Callable, Iterator
 from typing import Any, TypeVar
+import pipe as p
 
 T = TypeVar("T")
+U = TypeVar("U")
 
 def parseNum(n: str) -> int:
     return int(n.strip().lstrip("+"))
@@ -9,13 +11,15 @@ def parseNum(n: str) -> int:
 """
 Matrixes or Board utils
 """
-def boardWidth(board: list[list[str]]) -> int:
+def boardWidth(board: list[list[Any]]) -> int:
     return len(board)
-def boardHeight(board: list[list[str]]) -> int:
+def boardHeight(board: list[list[Any]]) -> int:
     return len(board[0])
 
-def parseBoard(text: str):
-    map(lambda g: list(g), text.split("\n"))
+def parseBoard(text: str) -> list[list[str]]:
+    return transpose(list(map(lambda g: list(g), text.split("\n")[:-1])))
+def transpose(board: list[list[T]]) -> list[list[T]]:
+    return [[board[y][x] for y in range(len(board[0]))] for x in range(len(board))]
 def createBoard(width: int, height: int, init: Callable[[int, int], T] = (lambda x, y: 0)) -> list[list[T]]:
     return [[init(x, y) for y in range(height)] for x in range(width)]
 def printBoard(board: list[list[Any]]):
@@ -24,11 +28,18 @@ def printBoard(board: list[list[Any]]):
         for x in range(boardWidth(board)):
             line += str(board[x][y])
         print(line)
-def posIter(board: list[list[Any]]):
+def posIter(board: list[list[Any]]) -> Iterator[tuple[int, int]]:
     return (
         (x, y)
         for x in range(boardWidth(board))
         for y in range(boardHeight(board))
+    )
+def mapBoard(mm: Callable[[int, int, T], U], board: list[list[T]]) -> list[list[U]]:
+    return createBoard(boardWidth(board), boardHeight(board), lambda x, y: mm(x, y, board[x][y]))
+def linearBoard(board: list[list[T]]) -> Iterator[tuple[int, int, T]]:
+    return (
+        (x, y, board[x][y])
+        for (x, y) in posIter(board)
     )
 
 def day#DAY_NUMBER#(contents: str):
