9758708d9bb0433871835aa5494139cae7c2e38e
Heavenston <malolegendrelemaire@@gmail.com>
2023-12-01 06:54:55 +0100

    day1

--- 2023/Day01/Ruby/part1.rb
+++ 2023/Day01/Ruby/part1.rb
@@ -1,9 +1,23 @@
 input = STDIN.read
 lines = input.split("\n")
 
+# vals = lines.map { |x| Integer(x) }
+vals = lines
+
+def numeric?(lookAhead)
+  lookAhead.match?(/[[:digit:]]/)
+end
+
 total = 0
-lines.each { |line|
-  printf "%d: %s\n", total, line
-  total += 1
+vals.each { |val|
+  # printf "%d: %s\n", total, val
+  a, *rest, b = val.chars.filter { |x| numeric?(x) }
+  if a == nil
+    a = b
+  end
+  if b == nil
+    b = a
+  end
+  total += Integer(a + b)
 }
 puts total
--- 2023/Day01/Ruby/part2.rb
+++ 2023/Day01/Ruby/part2.rb
@@ -1,9 +1,33 @@
 input = STDIN.read
 lines = input.split("\n")
 
+# vals = lines.map { |x| Integer(x) }
+vals = lines
+
+def numeric?(lookAhead)
+  lookAhead.match?(/[[:digit:]]/)
+end
+
 total = 0
-lines.each { |line|
-  printf "%d: %s\n", total, line
-  total += 1
+vals.each { |val|
+  val = val.gsub(/one/, "o1ne")
+  val = val.gsub(/two/, "t2wo")
+  val = val.gsub(/three/, "thr3ee")
+  val = val.gsub(/four/, "fo4ur")
+  val = val.gsub(/five/, "fi5ve")
+  val = val.gsub(/six/, "s6ix")
+  val = val.gsub(/seven/, "sev7en")
+  val = val.gsub(/eight/, "eig8ht")
+  val = val.gsub(/nine/, "ni9ne")
+  # puts val
+  # printf "%d: %s\n", total, val
+  a, *rest, b = val.chars.filter { |x| numeric?(x) }
+  if a == nil
+    a = b
+  end
+  if b == nil
+    b = a
+  end
+  total += Integer(a + b)
 }
 puts total
