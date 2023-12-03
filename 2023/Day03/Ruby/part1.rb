a08429a99cfe432cdbb27bbee8f5e659d60b22bc
Heavenston <malolegendrelemaire@@gmail.com>
2023-12-03 06:36:17 +0100

    Day 3

--- 2023/Day03/Ruby/part1.rb
+++ 2023/Day03/Ruby/part1.rb
@@ -1,9 +1,65 @@
 input = STDIN.read
 lines = input.split("\n")
 
-total = 0
-lines.each { |line|
-  printf "%d: %s\n", total, line
-  total += 1
+def numeric?(lookAhead)
+  lookAhead.match?(/[[:digit:]]/)
+end
+
+h = lines.map { |x| x.chars }
+
+# puts h.to_s
+
+alls = {}
+
+(0...h.length).each { |rowi|
+  row = h[rowi]
+  (0...row.length).each { |coli|
+    if not numeric?(row[coli])
+      next
+    end
+
+    nstart = coli
+    while nstart > 0 and numeric?(row[nstart - 1])
+      nstart -= 1
+    end
+    nend = nstart
+    while row[nend + 1] != nil and numeric?(row[nend + 1])
+      nend += 1
+    end
+
+    (-1..1).each { |drow|
+      (-1..1).each { |dcol| 
+        r = rowi + drow
+        c = coli + dcol
+        if r < 0 or r >= h.length
+          next
+        end
+        if c < 0 or c >= h[r].length
+          next
+        end
+        # if not h[r][c] == nil
+        #   next
+        # end
+        unless numeric?(h[r][c]) or h[r][c] == "."
+          alls[[rowi, nstart]] = nend
+          # printf "%d %d\n", nstart, nend
+        end
+      }
+    }
+  }
 }
-puts total
+
+t = 0
+alls.each { |key, nend|
+  if not key
+    next
+  end
+  row, nstart = key
+  k = ""
+  (nstart..nend).each { |i|
+    k += h[row][i]
+  }
+  t += Integer(k)
+}
+
+puts t
--- 2023/Day03/Ruby/part2.rb
+++ 2023/Day03/Ruby/part2.rb
@@ -1,9 +1,81 @@
 input = STDIN.read
 lines = input.split("\n")
 
-total = 0
-lines.each { |line|
-  printf "%d: %s\n", total, line
-  total += 1
+def numeric?(lookAhead)
+  lookAhead.match?(/[[:digit:]]/)
+end
+
+h = lines.map { |x| x.chars }
+
+# puts h.to_s
+
+alls = {}
+
+def get_num(h, rowi, coli)
+  nstart = coli
+  while nstart > 0 and numeric?(h[rowi][nstart - 1])
+    nstart -= 1
+  end
+  nend = nstart
+  while h[rowi][nend + 1] != nil and numeric?(h[rowi][nend + 1])
+    nend += 1
+  end
+  [nstart, nend]
+end
+
+(0...h.length).each { |rowi|
+  row = h[rowi]
+  (0...row.length).each { |coli|
+    if row[coli] != "*"
+      next
+    end
+    
+    nums = {}
+
+    (-1..1).each { |drow|
+      (-1..1).each { |dcol| 
+        r = rowi + drow
+        c = coli + dcol
+        if r < 0 or r >= h.length
+          next
+        end
+        if c < 0 or c >= h[r].length
+          next
+        end
+
+        if not numeric?(h[r][c])
+          next
+        end
+        
+        nstart, nend = (get_num h, r, c)
+        nums[[r, nstart]] = [r, c, nstart, nend]
+      }
+    }
+
+    # puts nums.to_s
+    if nums.length != 2
+      next
+    end
+
+    alls[[rowi, coli]] = nums
+  }
 }
-puts total
+
+t = 0
+alls.each { |key, nums|
+  prod = 1
+
+  nums.each_value { |v|
+    rowi, coli, nstart, nend = v
+    k = ""
+    (nstart..nend).each { |i|
+      k += h[rowi][i]
+    }
+    # puts k
+    prod *= Integer(k)
+  }
+
+  t += prod
+}
+
+puts t
