3759410cc399f94f6ed1ba3124ebc2ee46dd9e2d
Heavenston <malolegendrelemaire@@gmail.com>
2023-12-19 05:58:26 +0100

    graph in base

--- 2023/ADayX/Ruby/part2.rb
+++ 2023/ADayX/Ruby/part2.rb
@@ -1,10 +1,19 @@
 require "matrix"
+require "rgl/base"
+require "rgl/adjacency"
+require "rgl/dot"
 
 input = STDIN.read
 lines = input.split("\n")
 
-h = {}
-h.default = nil
+$x = lines.map { |x| x.chars.map { |j| j } }
+$dest = Vector[$x.length - 1, $x[0].length - 1]
+
+$g = RGL::DirectedAdjacencyGraph::new
+$g = RGL::AdjacencyGraph::new
+
+$h = {}
+$h.default = false
 
 total = 0
 
